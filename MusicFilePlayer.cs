436bd6a2404e6405faa2ca2784e926056e2ea4ca
slooooth <47870838+slooooth@users.noreply.github.com>
2020-09-29 01:17:57 -0500

    9.29.20.3

---
+++ MusicFilePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using NAudio.Wave;
+
+namespace MusicPlayer
+{
+    class MusicFilePlayer
+    {
+        public static void PlayMP3File()
+        {
+            
+        }
+        public static void PlayWAVFile()
+        { 
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using NAudio.Wave;
 
 namespace MusicPlayer
 {
